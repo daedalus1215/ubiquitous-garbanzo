78c5b3ac277cc9f6ebe87abb6c725c04ff9798c2
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-11 22:51:50 -0400

    car-2: Adding reports

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ReportsController } from './reports.controller';
+import { Module } from "@nestjs/common";
+import { ReportsController } from "./reports.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Report } from "./report.entity";
 
 @Module({
-  controllers: [ReportsController]
+  imports: [TypeOrmModule.forFeature([Report])],
+  controllers: [ReportsController],
 })
 export class ReportsModule {}
