cd8273244ec4981ff00cb7b00daeb0e614f2026f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-13 20:59:32 -0400

    car-3: Filling in more user logic around controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { IsEmail } from 'class-validator';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -11,5 +13,25 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email);
+    }
+
+    @Delete('/:id')
+    deleteUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id:string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body);
     }
 }
