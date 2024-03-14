78c81e97d3e35d635b315a51d699496b481ff3da
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-13 20:44:54 -0400

    car-3: add more functionality to service.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,7 +11,23 @@ export class UsersService {
 
     create(email: string, password: string) {
         const entity = this.repo.create({ email, password });
-        
+
         return this.repo.save(entity);
+    }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({ id });
+    }
+
+    find(email: string) {
+        return this.repo.find({ where: { email } })
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('user not found');
+        }
+        return this.repo.save(Object.assign(user, attrs));
     }
 }
