1a1375aa93aacc26649ba2f484ab247d10ae52fe
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-15 09:45:00 -0400

    car-4: serialize outgoing props for the userdto in the SerializerInterceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,12 +1,15 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
 import { Observable, map } from "rxjs";
+import { UserDto } from "src/users/dtos/user.dto";
 
 
 export class SerializeInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        console.log('Im running before the handler', context);
         return next.handle().pipe(map((data: any) => {
-            console.log('Im running before response is set out!')
+            return plainToClass(UserDto, data, {
+                excludeExtraneousValues: true
+            });
         }));
     }
 }
