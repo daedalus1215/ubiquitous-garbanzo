a83d8906cd62f08c291fd061ed20f63b3401d5e5
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-16 10:44:49 -0400

    car-5: Clean up the serialization in user controller

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,8 +1,11 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
 import { plainToClass } from "class-transformer";
 import { Observable, map } from "rxjs";
 import { UserDto } from "src/users/dtos/user.dto";
 
+export const Serialize = (dto: any) => {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
     constructor(private dto: any) {
