ac1d522c15f3c5717d8ac17da4a1d8e111f911bc
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-19 10:01:46 -0400

    Update user controller spec. Begin to mock in the services with fakes.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,26 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { UsersController } from "./users.controller";
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './services/users.service';
+import { AuthService } from './services/auth.service';
+import { User } from './user.entity';
 
-describe("UsersController", () => {
+describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => Promise.resolve({id, email: 'asdf@asdf.com', password: 'mypassword'}) as Promise<User>,
+      find: (email: string) => Promise<User[]>,
+      remove: (id: number) => Promise<User>,
+      update: () => {}
+    };
+    fakeAuthService = {
+      signup: () => {},
+      signin: () => {}
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
@@ -12,7 +28,7 @@ describe("UsersController", () => {
     controller = module.get<UsersController>(UsersController);
   });
 
-  it("should be defined", () => {
+  it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 });
