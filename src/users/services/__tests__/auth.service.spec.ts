2f4b5ff0c005b3a2fc273ca6f07820c8c8e968d3
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-18 22:24:16 -0400

    add test for salt and hashing password.

--- src/users/services/__tests__/auth.service.spec.ts
+++ src/users/services/__tests__/auth.service.spec.ts
@@ -12,9 +12,10 @@ describe('AuthService', () => {
     beforeEach(async () => {
         const fakeUsersService: Partial<UsersService> = {
             find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
+            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
+            update: (id: number, attrs: Partial<User>) => Promise.resolve({ id: 1, ...attrs } as User)
         };
-    
+
         const module = await Test.createTestingModule({
             providers: [
                 AuthService,
@@ -24,16 +25,29 @@ describe('AuthService', () => {
                 }
             ],
         }).compile();
-    
+
         target = module.get(AuthService);
     });
-    
+
     it('can create an instance of auth service', async () => {
         // Arrange
-        
+
         // Act
-    
+
         // Assert
         expect(target).toBeDefined();
+    });
+
+    it('creates a new user with a salted and hashed password', async () => {
+        // Arrange
+
+        // Act
+        const actual = await target.signup('asdf@asdf.com', 'asdf');
+
+        // Assert
+        expect(actual.password).not.toEqual('asdf');
+        const [salt, hash] = actual.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
     });
 });
