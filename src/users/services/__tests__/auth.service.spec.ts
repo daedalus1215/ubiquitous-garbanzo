d6768ea2f615cba535592b5fe8ac0995bb4f4660
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-18 22:28:56 -0400

    add test for throwing error if email not in system.

--- src/users/services/__tests__/auth.service.spec.ts
+++ src/users/services/__tests__/auth.service.spec.ts
@@ -2,15 +2,17 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "../auth.service";
 import { UsersService } from "../users.service";
 import { User } from "src/users/user.entity";
+import { BadRequestException } from "@nestjs/common";
 
 
 
 describe('AuthService', () => {
     // Arrange
     let target: AuthService;
+    let fakeUsersService: Partial<UsersService>;
 
     beforeEach(async () => {
-        const fakeUsersService: Partial<UsersService> = {
+        fakeUsersService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
             update: (id: number, attrs: Partial<User>) => Promise.resolve({ id: 1, ...attrs } as User)
@@ -28,26 +30,41 @@ describe('AuthService', () => {
 
         target = module.get(AuthService);
     });
-
-    it('can create an instance of auth service', async () => {
-        // Arrange
-
-        // Act
-
-        // Assert
-        expect(target).toBeDefined();
+    describe('exists', () => {
+        it('should create an instance of auth service', async () => {
+            // Arrange
+    
+            // Act
+    
+            // Assert
+            expect(target).toBeDefined();
+        });
     });
-
-    it('creates a new user with a salted and hashed password', async () => {
-        // Arrange
-
-        // Act
-        const actual = await target.signup('asdf@asdf.com', 'asdf');
-
-        // Assert
-        expect(actual.password).not.toEqual('asdf');
-        const [salt, hash] = actual.password.split('.');
-        expect(salt).toBeDefined();
-        expect(hash).toBeDefined();
+    
+    describe('#signup', () => {
+        it('should creates a new user with a salted and hashed password', async () => {
+            // Arrange
+    
+            // Act
+            const actual = await target.signup('asdf@asdf.com', 'asdf');
+    
+            // Assert
+            expect(actual.password).not.toEqual('asdf');
+            const [salt, hash] = actual.password.split('.');
+            expect(salt).toBeDefined();
+            expect(hash).toBeDefined();
+        });
+        
+        it('should throws an error if user signs up with email that is in use', async () => {
+            // Arrange
+    
+            // Act
+            fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    
+            // Assert
+            await expect(target.signup('asdf@asdf.com', 'asdf'))
+                .rejects
+                .toThrow(BadRequestException);
+        });
     });
 });
