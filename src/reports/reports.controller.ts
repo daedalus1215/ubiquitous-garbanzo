3fb6d9378c28926e5ed8da366383f11090626a5d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-21 21:53:30 -0400

    add get estimate dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from "@nestjs/common";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { AuthGuard } from "src/guards/auth.guard";
 import { ReportsService } from "./reports.service";
@@ -8,10 +8,17 @@ import { Serialize } from "src/interceptors/serialize.interceptor";
 import { ReportDto } from "./dtos/report.dto";
 import { ApproveReportDto } from "./dtos/approve-report.dto";
 import { AdminGuard } from "src/guards/admin.guard";
+import { GetEstimateDto } from "./dtos/get-estimate.dto";
 
 @Controller("reports")
 export class ReportsController {
     constructor(private reportsService: ReportsService) { }
+
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+        
+    }
 
     @Post()
     @UseGuards(AuthGuard)
